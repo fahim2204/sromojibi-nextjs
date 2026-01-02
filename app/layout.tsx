e4c454211b4b7ccb373740bfc2b33150fba92759
Fahim Faisal <fahimfaisal1998@gmail.com>
2026-01-02 16:53:13 +0600

    feat: add Google AdSense account to metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,6 +80,9 @@ export const metadata: Metadata = {
       'max-snippet': -1,
     },
   },
+  other: {
+    "google-adsense-account": "ca-pub-7554208332966422",
+  },
 };
 
 export default function RootLayout({
